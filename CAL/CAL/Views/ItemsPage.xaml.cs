f77eccf047d7a07af29a864425d59368a9ff766f
John Yenter-Briars <jyenterbriars@root16.com>
2022-03-23 19:21:28 -0500

    cal lives again

---
+++ CAL/CAL/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CAL.Models;
+using CAL.ViewModels;
+using CAL.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAL.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ CAL/CAL/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CAL.Models;
+using CAL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAL.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
