f99672e6fb1802e51a94de7bd61862cf455e2160
John Yenter-Briars <jyenterbriars@root16.com>
2022-04-27 08:59:36 -0500

    fix calendar header

---
+++ CAL/CAL/Controls/CalendarHeader.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAL.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarHeader : DataTemplate
+    {
+        public CalendarHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
