d9e0cc5b3dc7579f40efb6f371cb1ddd1f50aa56
John Yenter-Briars <jyenterbriars@root16.com>
2022-05-27 08:51:56 -0500

    initial commit of maui basic app

---
+++ CAL/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CAL;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
