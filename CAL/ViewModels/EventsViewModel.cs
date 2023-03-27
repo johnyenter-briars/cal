d458588b7b0183bc05b2aa8374e85a22da0be0c1
John Yenter-Briars <jyenterbriars@root16.com>
2023-03-27 11:30:27 -0500

    inital POC for scrollview

--- CAL/ViewModels/EventsViewModel.cs
+++ CAL/ViewModels/EventsViewModel.cs
@@ -11,40 +11,58 @@ using System.Windows.Input;
 
 namespace CAL.ViewModels
 {
-	//TODO: fix events page - CalClient needs to be an observable
-	public class EventsViewModel : BaseViewModel
-	{
-		public ObservableCollection<Event> Events { get; }
-		public Command LoadEventsCommand { get; }
-		public Command AddEventCommand { get; }
-		public ICommand EventSelectedCommand => new Command(async (item) => await ExecuteEventSelectedCommand(item));
+    public class EventsViewModel : BaseViewModel
+    {
+        bool fetchingNewPagedData = false;
+        public ObservableCollection<Event> Events { get; }
+        public Command LoadEventsCommand { get; }
+        private int currentPage = 0;
+        public ICommand EventSelectedCommand => new Command(async (item) => await ExecuteEventSelectedCommand(item));
+        public ICommand RemainingItemsThresholdReachedCommand => new Command(async () => await OnCollectionViewRemainingItemsThresholdReached());
 
-		public EventsViewModel()
-		{
-			Title = "Events";
-			//Events = EventDataStore.GetAsObservable();
+        public EventsViewModel()
+        {
+            Events = new ObservableCollection<Event>();
+            LoadEventsCommand = new Command(async () => await OnLoadEvents());
+        }
 
-			AddEventCommand = new Command(OnAddEvent);
+        public async void Refresh()
+        {
+            await OnLoadEvents();
+        }
+        private async Task OnLoadEvents(int page = 0)
+        {
+            var events = await CalClientSingleton.GetEventsPageAsync(page);
+            Events.Clear();
+            foreach (var e in events.Events)
+            {
+                Events.Add(e);
+            }
+        }
+        private async Task ExecuteEventSelectedCommand(object item)
+        {
+            if (item is Event e)
+            {
+                //var startUnixTimeSeconds = ((DateTimeOffset)e.StartTime.ToUniversalTime()).ToUnixTimeSeconds();
+                //var endUnixTimeSeconds = ((DateTimeOffset)e.EndTime.ToUniversalTime()).ToUnixTimeSeconds();
+                //await Shell.Current.GoToAsync($@"{nameof(EditEventPage)}?{nameof(EditEventViewModel.StartTimeUnixSeconds)}={startUnixTimeSeconds}&{nameof(EditEventViewModel.EndTimeUnixSeconds)}={endUnixTimeSeconds}&{nameof(EditEventViewModel.Id)}={e.Id}&{nameof(EditEventViewModel.Name)}={e.Name}&{nameof(EditEventViewModel.Description)}={e.Description}");
+            }
+        }
+        public async Task OnCollectionViewRemainingItemsThresholdReached()
+        {
+            if (fetchingNewPagedData) return;
 
-		}
+            fetchingNewPagedData = true;
 
-		public void OnAppearing()
-		{
-			IsBusy = true;
-		}
-		private async void OnAddEvent(object obj)
-		{
-			await Shell.Current.GoToAsync(nameof(EditEventPage));
-		}
+            currentPage++;
 
-		private async Task ExecuteEventSelectedCommand(object item)
-		{
-			if (item is Event e)
-			{
-				var startUnixTimeSeconds = ((DateTimeOffset)e.StartTime.ToUniversalTime()).ToUnixTimeSeconds();
-				var endUnixTimeSeconds = ((DateTimeOffset)e.EndTime.ToUniversalTime()).ToUnixTimeSeconds();
-				await Shell.Current.GoToAsync($@"{nameof(EditEventPage)}?{nameof(EditEventViewModel.StartTimeUnixSeconds)}={startUnixTimeSeconds}&{nameof(EditEventViewModel.EndTimeUnixSeconds)}={endUnixTimeSeconds}&{nameof(EditEventViewModel.Id)}={e.Id}&{nameof(EditEventViewModel.Name)}={e.Name}&{nameof(EditEventViewModel.Description)}={e.Description}");
-			}
-		}
-	}
+            var events = await CalClientSingleton.GetEventsPageAsync(currentPage);
+            foreach (var e in events.Events)
+            {
+                Events.Add(e);
+            }
+
+            fetchingNewPagedData = false;
+        }
+    }
 }
