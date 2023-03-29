2e81dc539d241c9f5b699a77ec6eda19932e02b4
John Yenter-Briars <jyenterbriars@root16.com>
2023-03-29 07:28:14 -0500

    fix up events list icons

--- CAL/Models/Event.cs
+++ CAL/Models/Event.cs
@@ -4,22 +4,23 @@ using CAL.Client.Models.Cal;
 
 namespace CAL.Models
 {
-	public class CalendarEvent : BaseObservableModel
-	{
-		public Guid Id { get; set; }
-		public DateTime StartTime { get; set; }
-		public DateTime EndTime { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public Guid CalUserId { get; set; }
-		public Guid? SeriesId { get; set; }
-		public Guid CalendarId { get; set; }
-		public Color Color { get; set; }
-		public EntityType EntityType => EntityType.Event;
-		public override string ToString() => $"{Name}-LOCAL START: {StartTime}- UTC START: {StartTime.ToUniversalTime()}-{EndTime}";
-		public string SeriesName { get; set; }
-		public bool IsPartOfSeries => SeriesName != null;
-		public int NumTimesNotified { get; set; }
-		public bool ShouldNotify { get; set; }
-	}
+    public class CalendarEvent : BaseObservableModel
+    {
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CalUserId { get; set; }
+        public Guid? SeriesId { get; set; }
+        public Guid CalendarId { get; set; }
+        public Color Color { get; set; }
+        public EntityType EntityType => EntityType.Event;
+        public override string ToString() => $"{Name}-LOCAL START: {StartTime}- UTC START: {StartTime.ToUniversalTime()}-{EndTime}";
+        public string SeriesName { get; set; }
+        public bool IsPartOfSeries => SeriesName != null;
+        public bool IsSingularEvent => SeriesName == null;
+        public int NumTimesNotified { get; set; }
+        public bool ShouldNotify { get; set; }
+    }
 }
