bc877ffd28443a8b7eb15903b3303f90caacddfc
John Yenter-Briars <johnyenterbriars@protonmail.com>
2025-09-01 13:17:50 -0500

    ui updates

---
+++ CAL/Views/ProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CAL.Client;
+using CAL.Managers;
+using CAL.Models;
+using CAL.ViewModels;
+using System;
+using System.ComponentModel;
+
+namespace CAL.Views
+{
+	public partial class ProfilePage : ContentPage
+	{
+		public ProfilePage()
+		{
+			//InitializeComponent();
+			InitializeComponent();
+			BindingContext = new ProfileViewModel();
+		}
+        protected override void OnAppearing()
+        {
+            var bc = (ProfileViewModel)BindingContext;
+
+            bc?.Refresh();
+        }
+	}
+}
